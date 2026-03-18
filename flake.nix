b4406ad4b6c6a0e3f83df1930a9de53b4d416c59
= <p3dr0c4@live.com>
2026-03-18 08:20:01 -0300

    init rustlings

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Rustix Cube";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+            # Add other deps here, e.g.:
+            # pkgs.openssl
+          ];
+        };
+      }
+    );
+}
